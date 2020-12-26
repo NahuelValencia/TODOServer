3749d3b183d9a290c89f710eb1650c1ba4f53323
Nahuel Valencia <nahuel.valencia21@gmail.com>
2020-12-26 19:45:45 -0300

    initialCommit

---
+++ src/Application.kt
@@ -0,0 +1,53 @@
+package com.nahuelvalencia
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.sessions.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Locations) {
+    }
+
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+
+    }
+}
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+
+@Location("/type/{name}") data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
+
+data class MySession(val count: Int = 0)
+
